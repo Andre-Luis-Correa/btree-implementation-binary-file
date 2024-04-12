77d8b1668a3e34c418b2355194632b876e3c5ad9
Andre Correa <andreluiscorrea2003@gmail.com>
2024-04-12 14:26:55 -0300

    feat: remover produto

--- remocao/produto_remocao.c
+++ remocao/produto_remocao.c
@@ -26,7 +26,7 @@ int eh_raiz(ARVOREB * r){
 
 int underflow(ARVOREB * r){
     if (!vazia(r)){
-        if (r->num_chaves < ORDEM/2 && !eh_raiz(r) )
+        if (r->num_chaves < ORDEM/2 && !eh_raiz(r))
             return 1;
         return 0;
     }
@@ -36,139 +36,75 @@ int underflow(ARVOREB * r){
 void remover_produto(ARQUIVOS files){
     CABECALHO_INDICES * cab_indices = le_cabecalho_indices(files.file_indices);
 
-    int codigo, ha_codigo;
+    int codigo, pos_codigo, pos_raiz, pos_esq, pos_dir;
+
     printf ("Insira o codigo a ser removido: ");
     scanf ("%d", &codigo);
 
-    ha_codigo = buscar_no(files.file_indices, codigo, cab_indices->pos_raiz);
+    pos_codigo = buscar_no(files.file_indices, codigo, cab_indices->pos_raiz);
 
     if(cab_indices->pos_raiz == -1){
         printf("A arvore encontra-se vazia!\n");
         free(cab_indices);
         return;
-    } else if (ha_codigo == -1) {
+    } else if (pos_codigo == -1) {
         printf("Produto nao encontrado na arvore!\n");
         free(cab_indices);
         return;
     }
 
     ARVOREB * raiz = ler_no(files.file_indices, cab_indices->pos_raiz);
-    ARVOREB * no_removido = remover(files, raiz, codigo);
+    pos_raiz = cab_indices->pos_raiz;
+    pos_esq = ;
+    pos_dir = ;
+    remover(files, raiz, pos_raiz, pos_esq, pos_dir, codigo);
 
-    free(no_removido);
     free(raiz);
     free(cab_indices);
 }
 
-void realizar_balanceamento(FILE * files_indices, ARVOREB * r)
-{
-    if (isEmprestimo(arqArv,r)){//se pode emprestar, faz a redistribuição
-        redistribuir(arqArv,r);
+int pode_emprestar(ARVOREB * r){
+    if (r != NULL){
+        if (r->num_chaves > ORDEM/2)
+            return 1;
+        return 0;
+    }
+    return 0;
+}
+
+ARVOREB * find_irmao_esq(FILE* file_indices, ARVOREB * filho){
+    return NULL;
+}
+
+ARVOREB * find_irmao_dir(FILE *pIobuf, ARVOREB *pNode) {
+    return NULL;
+}
+
+int is_emprestimo(FILE* file_indices, ARVOREB * r){
+
+    ARVOREB * esq = find_irmao_esq(file_indices,r);
+    ARVOREB * dir = find_irmao_dir(file_indices,r);
+
+    if (pode_emprestar(esq) || pode_emprestar(dir))
+    {
+        free(esq);
+        free(dir);
+        return 1;
+    }
+    free(esq);
+    free(dir);
+    return 0;
+
+}
+
+void realizar_balanceamento(ARQUIVOS files, ARVOREB * r){
+    if (is_emprestimo(files.file_indices, r)){//se pode emprestar, faz a redistribuição
+        redistribuir(arqArv, r);
     } else {//nao da pra emprestar, faz concatenação
-        concatenar(arqArv,r);
+        concatenar(arqArv, r);
     }
 }
 
-ARVOREB * remove_produto_file(ARQUIVOS files, ARVOREB * r, int codigo) {
+void remover(ARQUIVOS files, int pos_raiz, int pos_esq, int pos_dir, int codigo){
 
-    if(!vazia(r)) {
-        int i = 0;
-        for (i = 0; i < r->num_chaves; i++) { //varre o vetor em busca da chave se achar dá break
-            if (r->chave[i] == codigo) {
-                break;
-            } else if (r->chave[i] > codigo) { //compara o valor da arvore r atual com a chave a ser encontrada, se for maior busca no filho
-                ARVOREB * filho_pos_i = ler_no(files.file_indices, r->filho[i]);
-                filho_pos_i = remover(files, filho_pos_i, codigo);
-                if (underflow(filho_pos_i)) {
-                    realizar_balanceamento(files.file_indices, filho_pos_i);
-                } // Etou AQUIII
-                if (filho_i != NULL) {
-                    r->filho[i] = filho_i->posicao;
-                    free(filho_i);
-                } else {
-                    r->filho[i] = -1;
-                }
-
-                int pos = r->posicao;
-                free(r);//libera o nó anterior
-                r = le_no(arqArv, pos);//atualiza o nó que foi mudado pelo balancear.
-                //escreve_no(arqArv, r, r->posicao);
-                //return r;
-            }
-        }
-        if (i >=
-            r->numChaves) { // nao encntrou a chave neste nó,todos os numero aqui sao menores que a chave, por isso procura no filho mais a direita
-            arvoreB *filho_i = le_no(arqArv, r->filho[i]);
-            filho_i = remover(arqProd, arqArv, filho_i, x);
-            if (filho_i != NULL) {
-                r->filho[i] = filho_i->posicao;
-            } else {
-                r->filho[i] = -1;
-            }
-            if (underflow(filho_i)) {
-                balancear(arqArv, filho_i);
-            }
-        } else { //i é menor que núermo de chaves: achou algo neste nó,quando encontra a chave, sempre libera posições do arquivo de produto aqui
-            if (eh_folha(r)) {
-                addPosLivreProd(arqProd, r->ptDados[i]);
-                while (i + 1 < r->numChaves) {
-                    r->chave[i] = r->chave[i + 1];
-                    r->ptDados[i] = r->ptDados[i + 1];
-                    i++;
-                }
-                r->numChaves--;//diminui uma chave;
-                escreve_no(arqArv, r, r->posicao);
-
-            } else if (!eh_folha(r)) { //é no interno, não é folha
-                addPosLivreProd(arqProd, r->ptDados[i]);
-
-                arvoreB *filho_i = le_no(arqArv, r->filho[i]);
-                int pt = -1;
-                r->chave[i] = maximoRemove(arqArv, filho_i, &pt);
-                r->ptDados[i] = pt;
-                escreve_no(arqArv, r, r->posicao);
-                filho_i = remover(arqProd, arqArv, filho_i, r->chave[i]);
-                if (filho_i != NULL) {
-                    r->filho[i] = filho_i->posicao;
-                } else {
-                    r->filho[i] = -1;
-                }
-                if (underflow(filho_i)) {
-                    balancear(arqArv, filho_i);
-                }
-
-            }
-
-        }
-        //testa se a raiz ficou vazia.
-        int pos = r->posicao;
-        free(r);
-        r = le_no(arqArv, pos);
-        if (r != NULL) {
-
-            if (eh_raiz(r) && r->numChaves <= 0) {
-                arvoreB *aux = le_no(arqArv, r->filho[0]);
-
-                if (aux != NULL) {
-                    aux->pai = -1;
-                    mudarRaiz(arqArv, aux->posicao);
-                    escreve_no(arqArv, aux, aux->posicao);
-                } else {
-                    mudarRaiz(arqArv, -1);
-                }
-                addPosLivreArv(arqArv, r->posicao);
-                free(r);
-                r = NULL;
-                return aux;
-            }
-        }
-
-        if (r != NULL) {
-
-            escreve_no(arqArv, r, r->posicao);
-        }
-        return r;
-    }
-
-    return NULL;
 }
--- remocao/produto_remocao.h
+++ remocao/produto_remocao.h
@@ -11,6 +11,11 @@ int vazia(ARVOREB * r);
 int eh_raiz(ARVOREB * r);
 int underflow(ARVOREB * r);
 void remover_produto(ARQUIVOS files);
-ARVOREB * remover(ARQUIVOS files, ARVOREB * r, int codigo);
+int pode_emprestar(ARVOREB * r);
+ARVOREB *find_irmao_esq(FILE * file_indices, ARVOREB * r);
+ARVOREB *find_irmao_dir(FILE * file_indices, ARVOREB * r);
+int is_emprestimo(FILE* file_indices, ARVOREB * r);
+void realizar_balanceamento(ARQUIVOS files, ARVOREB * r);
+void remover(ARQUIVOS files, int pos_raiz, int pos_esq, int pos_dir, int codigo);
 
 #endif //PRODUTO_REMOCAO_H
