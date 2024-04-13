cd80c169111ce05302a1499d18e6dbe81732a67c
Andre Correa <andreluiscorrea2003@gmail.com>
2024-04-12 22:19:15 -0300

    feat: remover produto

--- remocao/produto_remocao.c
+++ remocao/produto_remocao.c
@@ -5,8 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "produto_remocao.h"
-#include "../insercao/produto_insercao.h"
-#include "../impressao/produto_impressao.h"
 
 #define MIN (ORDEM/2 + ORDEM%2 - 1)
 
@@ -42,7 +40,7 @@ int mais_chaves_que_min(ARVOREB * r){
     return r->num_chaves > MIN;
 }
 
-int bus_pos_chave(ARVOREB * r, int codigo){
+int busca_pos_chave(ARVOREB * r, int codigo){
     int pos_codigo;
 
     // Encontra a pos dentro do nó
@@ -54,19 +52,39 @@ int bus_pos_chave(ARVOREB * r, int codigo){
     return -1;
 }
 
+void atualiza_lista_dados_livres(ARQUIVOS files, CABECALHO_DADOS * cab_dados, int pos_registro){
+
+    DADOS_REGISTRO * dados_atual = ler_registro(files.file_dados, cab_dados->pos_livre);
+
+    int pos_atual = cab_dados->pos_livre;
+
+    while(dados_atual->prox_livre != -1){
+        pos_atual = dados_atual->prox_livre;
+        dados_atual = ler_registro(files.file_dados, dados_atual->prox_livre);
+    }
+
+    dados_atual->prox_livre = pos_registro;
+    escreve_registro(files.file_dados, dados_atual, pos_atual);
+}
+
 void atualiza_no_remocao_folha(ARQUIVOS files, ARVOREB * folha, int pos_remocao, int pos_codigo){
     CABECALHO_DADOS * cab_dados = le_cabecalho_dados(files.file_dados);
 
     // A posição dos dados no arq de dados deve ser atualizado
     // folha->pt_dados[pos_codigo] => posição dos dados no file de dados
     int pos_registro = folha->pt_dados[pos_codigo];
-    cab_dados->pos_livre = pos_registro;
+
+    if(cab_dados->pos_livre == -1){
+        cab_dados->pos_livre = pos_registro;
+        // Atualiza o cab_dados com a posição livre
+        escreve_cabecalho_dados(files.file_dados, cab_dados);
+
+    }else {
+        atualiza_lista_dados_livres(files, cab_dados, pos_registro);
+    }
 
     printf("\n ---> CASO 1 - ANTES ATT - REMOCAO: \n");
     imprimir_no(folha);
-
-    // Atualiza o cab_dados com a posição livre
-    escreve_cabecalho_dados(files.file_dados, cab_dados);
 
     // Agora realizar a atualização do nó folha
     int i;
@@ -92,8 +110,45 @@ void remover_caso1(ARQUIVOS files, ARVOREB * no_a_remover, int codigo, int pos_r
     int pos_codigo, i;
 
     // Encontra a pos dentro do nó
-    pos_codigo = bus_pos_chave(no_a_remover, codigo);
+    pos_codigo = busca_pos_chave(no_a_remover, codigo);
     atualiza_no_remocao_folha(files, no_a_remover, pos_remocao, pos_codigo);
+}
+
+int buscar_chave_sucessora_folha(ARQUIVOS files, ARVOREB * no_a_remover, int pos_codigo, int * pos_no_sucessor){
+    int pos_chave_sucessora = no_a_remover->filho[pos_codigo + 1];
+    int chave_sucessora;
+
+    ARVOREB * no_sucessor = ler_no(files.file_indices, pos_chave_sucessora);
+
+    if(eh_folha(no_sucessor)){
+        *pos_no_sucessor = pos_chave_sucessora;
+        chave_sucessora = no_sucessor->chave[0];
+        printf("\n ----> Essa é a chave sucessora: %d\n", chave_sucessora);
+        free(no_sucessor);
+        return chave_sucessora;
+    }
+
+    return buscar_chave_sucessora_folha(files, no_sucessor, -1, pos_no_sucessor);
+}
+
+int remover_caso2(ARQUIVOS files, ARVOREB * no_a_remover, int codigo, int pos_remocao){
+    // Primeiramente, busca a posição da chave (dentro do nó) a ser removida no nó
+    int pos_codigo = busca_pos_chave(no_a_remover, codigo);
+    int pos_no_sucessor;
+
+    // Busca a chave sucessora para inseri-lá na pos_codigo encontrada
+    // Também retorna a posição no arq de indices do nó sucessor
+    int chave_sucessora = buscar_chave_sucessora_folha(files, no_a_remover, pos_codigo, &pos_no_sucessor);
+
+    // Preciso atualizar a folha que continha a chave sucessora
+    ARVOREB  * no_sucessor = ler_no(files.file_indices, pos_no_sucessor);
+    atualiza_no_remocao_folha(files, no_sucessor, pos_no_sucessor, 0);
+
+    // Realiza a substituição da chave removida pela chave sucessora
+    no_a_remover->chave[pos_codigo] = chave_sucessora;
+    escreve_no(files.file_indices, no_a_remover, pos_remocao);
+
+    return pos_no_sucessor;
 }
 
 void remover(ARQUIVOS files, int codigo, int pos_raiz, int pos_remocao){
@@ -103,9 +158,14 @@ void remover(ARQUIVOS files, int codigo, int pos_raiz, int pos_remocao){
     ARVOREB * no_a_remover = ler_no(files.file_indices, pos_remocao);
 
     // 1° CASO: a remoção é feita em um nó folha com número de chaves maior que o mínimo (ORDEM/2)
-    if(mais_chaves_que_min(no_a_remover) && eh_folha(no_a_remover)){
+    if( mais_chaves_que_min(no_a_remover) && eh_folha(no_a_remover) ){
         // Logo, apenas remove a chave do nó, realizando as alterações necessárias e gravando novamente no arquivo
         remover_caso1(files, no_a_remover, codigo, pos_remocao);
+
+    } else if ( !eh_folha(no_a_remover ) ) { // CASO 2°: a remoção é feita em um nó interno
+        // Logo, busca-se a chave sucessora e a insere no lugar da chave removida no nó interno
+        // Além disso, a função deve retornar a posição do nó chave sucessora, pois esse nó deve ser tratado após remoção
+        int pos_no_sucessor = remover_caso2(files, no_a_remover, codigo, pos_remocao);
     }
 
 
--- remocao/produto_remocao.h
+++ remocao/produto_remocao.h
@@ -6,11 +6,16 @@
 #define PRODUTO_REMOCAO_H
 
 #include "../produto/produto.h"
+#include "../insercao/produto_insercao.h"
+#include "../impressao/produto_impressao.h"
 
 void remover_produto(ARQUIVOS files);
-void remover(ARQUIVOS files, int codigo, int pos_raiz, int pos_remocao);
 int mais_chaves_que_min(ARVOREB * r);
-int bus_pos_chave(ARVOREB * r, int codigo);
+int busca_pos_chave(ARVOREB * r, int codigo);
+void atualiza_lista_dados_livres(ARQUIVOS files, CABECALHO_DADOS * cab_dados, int pos_registro);
 void atualiza_no_remocao_folha(ARQUIVOS files, ARVOREB * folha, int pos_remocao, int pos_codigo);
 void remover_caso1(ARQUIVOS files, ARVOREB * no_a_remover, int codigo, int pos_remocao);
+int buscar_chave_sucessora_folha(ARQUIVOS files, ARVOREB * no_a_remover, int pos_codigo, int * pos_no_sucessor);
+int remover_caso2(ARQUIVOS files, ARVOREB * no_a_remover, int codigo, int pos_remocao);
+void remover(ARQUIVOS files, int codigo, int pos_raiz, int pos_remocao);
 #endif //PRODUTO_REMOCAO_H
