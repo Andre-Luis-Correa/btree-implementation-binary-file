f8c365d8f0a7dca12a20a0572b914bcdaf355915
brunafeyh <bruna.feyh@unioeste.br>
2024-04-14 21:04:02 -0300

    fazendo impressao por niveis

---
+++ cmake-build-debug/CMakeFiles/trabalho_aed_3_bruna_andre.dir/menu/menu.c.o.d
@@ -0,0 +1,66 @@
+CMakeFiles/trabalho_aed_3_bruna_andre.dir/menu/menu.c.o: \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/menu.c \
+ /usr/include/stdc-predef.h /usr/include/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/stddef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endianness.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_mutex.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/menu.h \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/../file/file.h \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/../insercao/produto_insercao.h \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/../insercao/../produto/produto.h \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/../insercao/../produto/../file/file.h \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/../insercao/../cabecalho/cabecalho_indices.h \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/../insercao/../cabecalho/cabecalho_dados.h \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/../impressao/produto_impressao.h \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/../impressao/../produto/produto.h \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/../remocao/produto_remocao.h \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/../remocao/../produto/produto.h \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/../remocao/../insercao/produto_insercao.h \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/../remocao/../impressao/produto_impressao.h \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/../atualizacao/produto_atualizacao.h \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/../atualizacao/../produto/produto.h \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/../atualizacao/../cabecalho/cabecalho_indices.h \
+ /home/brunafeyh/CLionProjects/trabalho_aed_3_andre_bruna2/menu/../atualizacao/../cabecalho/cabecalho_dados.h
--- impressao/produto_impressao.h
+++ impressao/produto_impressao.h
@@ -7,9 +7,18 @@
 
 #include "../produto/produto.h"
 
+#define TAMANHO_FILA 100 // Ajuste o tamanho conforme necessário
+
+typedef struct {
+    ARVOREB *elementos[TAMANHO_FILA];
+    int frente; // Índice do primeiro elemento na fila
+    int tras;   // Índice do próximo espaço disponível na fila
+} Fila;
+
 void imprimir_lista_produtos(ARQUIVOS files, int pos_atual);
 void imprimir_informacoes_produto(ARQUIVOS files, int codido);
 void imprimir_no(ARVOREB *r);
+void imprimir_arvore(ARQUIVOS files);
 void imprimir_info_nos_chaves(ARQUIVOS files, int pos);
 
 #endif //PRODUTO_IMPRESSAO_H
