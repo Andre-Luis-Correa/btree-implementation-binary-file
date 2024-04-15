402725aa232295deba09f0ea851b4531035418b7
Andre Correa <andreluiscorrea2003@gmail.com>
2024-04-15 19:46:29 -0300

    feat: remover produto

--- atualizacao/produto_atualizacao.c
+++ atualizacao/produto_atualizacao.c
@@ -12,20 +12,17 @@ void atualizar_preco_produto(ARQUIVOS files, int codigo_produto){
     CABECALHO_INDICES * cab_indices = le_cabecalho_indices(files.file_indices);
     CABECALHO_DADOS * cab_dados = le_cabecalho_dados(files.file_dados);
 
-    imprimir_cabecalho_indices(cab_indices);
-    imprimir_cabecalho_dados(cab_dados);
-
     //a pos esta guarando o no em que esta elemento que possui esse codigo de produto
     int pos = buscar_no(files.file_indices, codigo_produto, cab_indices->pos_raiz);
 
     if (pos == -1) {
-        printf("Produto com código %d não encontrado.\n", codigo_produto);
+        printf("Produto com c?digo %d n?o encontrado.\n", codigo_produto);
         return;
     }
 
     ARVOREB * no = ler_no(files.file_indices, pos);
 
-    // procurar o indice do codigo dentro do nó
+    // procurar o indice do codigo dentro do n?
     int indice = -1;
     for (int i = 0; i < no->num_chaves; i++) {
         if (no->chave[i] == codigo_produto) {
@@ -35,7 +32,7 @@ void atualizar_preco_produto(ARQUIVOS files, int codigo_produto){
     }
 
     if (indice == -1) {
-        printf("Produto com código %d não encontrado.\n", codigo_produto);
+        printf("Produto com c?digo %d n?o encontrado.\n", codigo_produto);
         return;
     }
     // ler o registro de dados do produto
@@ -43,22 +40,22 @@ void atualizar_preco_produto(ARQUIVOS files, int codigo_produto){
 
     // atualizar o preco do produto
     float novo_preco;
-    printf("Insira o novo preço para o produto de código %d: ", codigo_produto);
+    printf("Insira o novo pre?o para o produto de c?digo %d: ", codigo_produto);
     scanf("%f", &novo_preco);
     registro->produto.preco = novo_preco;
 
     // Escrever o registro atualizado de volta no arquivo de dados
     escreve_registro(files.file_dados, registro, no->pt_dados[indice]);
 
-    // Liberar memória alocada
+    // Liberar mem?ria alocada
     free(no);
     free(registro);
 
-    printf("Preço do produto com código %d atualizado com sucesso.\n", codigo_produto);
+    printf("Pre?o do produto com c?digo %d atualizado com sucesso.\n", codigo_produto);
     free(cab_indices);
 }
 
-// Função para atualizar o estoque de um produto a partir do seu código
+// Fun??o para atualizar o estoque de um produto a partir do seu c?digo
 void atualizar_estoque_produto(ARQUIVOS files, int codigo_produto) {
     CABECALHO_INDICES * cab_indices = le_cabecalho_indices(files.file_indices);
     CABECALHO_DADOS * cab_dados = le_cabecalho_dados(files.file_dados);
@@ -69,13 +66,13 @@ void atualizar_estoque_produto(ARQUIVOS files, int codigo_produto) {
     int pos = buscar_no(files.file_indices, codigo_produto, cab_indices->pos_raiz);
 
     if (pos == -1) {
-        printf("Produto com código %d não encontrado.\n", codigo_produto);
+        printf("Produto com c?digo %d n?o encontrado.\n", codigo_produto);
         return;
     }
 
     ARVOREB * no = ler_no(files.file_indices, pos);
 
-    // Procurar o índice do código dentro do nó
+    // Procurar o ?ndice do c?digo dentro do n?
     int indice = -1;
     for (int i = 0; i < no->num_chaves; i++) {
         if (no->chave[i] == codigo_produto) {
@@ -85,7 +82,7 @@ void atualizar_estoque_produto(ARQUIVOS files, int codigo_produto) {
     }
 
     if (indice == -1) {
-        printf("Produto com código %d não encontrado.\n", codigo_produto);
+        printf("Produto com c?digo %d n?o encontrado.\n", codigo_produto);
         return;
     }
 
@@ -94,18 +91,18 @@ void atualizar_estoque_produto(ARQUIVOS files, int codigo_produto) {
 
     // Atualizar o estoque do produto
     int novo_estoque;
-    printf("Insira o novo estoque para o produto de código %d: ", codigo_produto);
+    printf("Insira o novo estoque para o produto de c?digo %d: ", codigo_produto);
     scanf("%d", &novo_estoque);
     registro->produto.estoque = novo_estoque;
 
     // Escrever o registro atualizado de volta no arquivo de dados
     escreve_registro(files.file_dados, registro, no->pt_dados[indice]);
 
-    // Liberar memória alocada
+    // Liberar mem?ria alocada
     free(no);
     free(registro);
 
-    printf("Estoque do produto com código %d atualizado com sucesso.\n", codigo_produto);
+    printf("Estoque do produto com c?digo %d atualizado com sucesso.\n", codigo_produto);
     free(cab_indices);
     free(cab_dados);
 }
--- menu/menu.c
+++ menu/menu.c
@@ -6,139 +6,187 @@
 #include "../remocao/produto_remocao.h"
 #include "../atualizacao/produto_atualizacao.h"
 
-// Exibe o cabeçalho do menu
-// Pré-condições: Nenhuma
-// Pós-condição: O cabeçalho do menu é exibido no console
+// Exibe o cabe?alho do menu
+// Pr?-condi??es: Nenhuma
+// P?s-condi??o: O cabe?alho do menu ? exibido no console
 void header_menu(){
-    printf("\nMenu:\n");
-    printf("1. Cadastrar Produto\n");
-    printf("2. Remover Produto\n");
-    printf("3. Atualizar Preco\n");
-    printf("4. Atualizar Estoque\n");
-    printf("5. Imprimir Informacoes de um Produto\n");
-    printf("6. Imprimir Lista de Todos os Produtos\n");
-    printf("7. Imprimir Arvore\n");
-    printf("8. Imprimir Lista de Livres do Arquivo de Indices\n");
-    printf("9. Imprimir Lista de Livres do Arquivo de Dados\n");
-    printf("10. Realizar Operacoes em Lote\n");
-    printf("11. Imprimir infos dos nós\n");
-    printf("0. Sair\n");
+    printf("\n");
+    printf("+--------------------------------------------------+\n");
+    printf("|                     Menu:                        |\n");
+    printf("+--------------------------------------------------+\n");
+    printf("| 1. Cadastrar Produto                             |\n");
+    printf("| 2. Remover Produto                               |\n");
+    printf("| 3. Atualizar Preco                               |\n");
+    printf("| 4. Atualizar Estoque                             |\n");
+    printf("| 5. Imprimir Informacoes de um Produto            |\n");
+    printf("| 6. Imprimir Lista de Todos os Produtos           |\n");
+    printf("| 7. Imprimir Arvore                               |\n");
+    printf("| 8. Imprimir Lista de Livres do Arquivo de Indices|\n");
+    printf("| 9. Imprimir Lista de Livres do Arquivo de Dados  |\n");
+    printf("| 10. Realizar Operacoes em Lote                   |\n");
+    printf("| 11. Imprimir infos dos nos                       |\n");
+    printf("| 0. Sair                                          |\n");
+    printf("+--------------------------------------------------+\n");
 }
 
+
 // Cadastra um novo produto
-// Pré-condições: Nenhuma
-// Pós-condição: Um novo produto é cadastrado
+// Pr?-condi??es: Nenhuma
+// P?s-condi??o: Um novo produto ? cadastrado
 void cadastrarProduto(ARQUIVOS files) {
-    printf("Operacao: Cadastrar Produto\n");
+    printf("\n---> Operacao: Cadastrar Produto\n");
     cadastrar_produto(files);
-    // Implemente a lógica para cadastrar um produto
+    // Implemente a l?gica para cadastrar um produto
 }
 
 // Remove um produto existente
-// Pré-condições: Nenhuma
-// Pós-condição: O produto é removido do cadastro
+// Pr?-condi??es: Nenhuma
+// P?s-condi??o: O produto ? removido do cadastro
 void removerProduto(ARQUIVOS files) {
-    printf("Operacao: Remover Produto\n");
+    printf("\n---> Operacao: Remover Produto\n");
     remover_produto(files);
-    // Implemente a lógica para remover um produto
+    // Implemente a l?gica para remover um produto
 }
 
-// Atualiza o preço de um produto
-// Pré-condições: Nenhuma
-// Pós-condição: O preço do produto é atualizado
+// Atualiza o pre?o de um produto
+// Pr?-condi??es: Nenhuma
+// P?s-condi??o: O pre?o do produto ? atualizado
 void atualizarPreco(ARQUIVOS files) {
-    printf("Operacao: Atualizar Preco\n");
-    int cod;
-    printf("Digite o codigo do produto de preco a ser atualizado:\n");
-    scanf("%d", &cod);
-    atualizar_preco_produto(files, cod);
-    // Implemente a lógica para atualizar o preço de um produto
-}
-
-// Atualiza o estoque de um produto
-// Pré-condições: Nenhuma
-// Pós-condição: O estoque do produto é atualizado
-void atualizarEstoque(ARQUIVOS files) {
-    printf("Operacao: Atualizar Estoque\n");
-    int cod;
-    printf("Digite o codigo do produto de estoque a ser atualizado:\n");
-    scanf("%d", &cod);
-    atualizar_estoque_produto(files, cod);
-}
-
-// Imprime as informações de um produto específico
-// Pré-condições: Nenhuma
-// Pós-condição: As informações do produto são impressas no console
-void imprimirInformacoes(ARQUIVOS files) {
-    printf("Operacao: Imprimir informacoes de um produto\n");
-    int codigo;
-
-    printf("Digite o código desejado para busca-lo: ");
-    scanf("%d", &codigo);
-    imprimir_informacoes_produto(files, codigo);
-}
-
-// Imprime a lista de todos os produtos cadastrados
-// Pré-condições: Nenhuma
-// Pós-condição: A lista de todos os produtos é impressa no console
-void imprimirListaProdutos(ARQUIVOS files) {
-    printf("Operacao: Imprimir Lista de Produtos\n\n");
+    // Implemente a l?gica para atualizar o pre?o de um produto
+    printf("\n---> Operacao: Atualizar Preco\n");
     CABECALHO_INDICES * cab_indices = le_cabecalho_indices(files.file_indices);
-    if(cab_indices->pos_raiz == -1) {
-        printf("Nao ha elementos na arvore\n");
-    }else {
-        imprimir_lista_produtos(files, cab_indices->pos_raiz);
+
+    if( cab_indices->pos_raiz == -1){
+        printf("---> A arvore esta vazia, nao e possivel realizar a operacao!\n");
+    } else {
+        int cod;
+        printf("---> Digite o codigo do produto de preco a ser atualizado:\n");
+        scanf("%d", &cod);
+        atualizar_preco_produto(files, cod);
     }
     free(cab_indices);
 }
-// Imprime a árvore de produtos
-// Pré-condições: Nenhuma
-// Pós-condição: A árvore de produtos é impressa no console
-void imprimirArvore(ARQUIVOS files) {
-    printf("Operacao: Imprimir Arvore\n");
-    imprimir_arvore(files);
-    // Implemente a lógica para imprimir a árvore de produtos
+
+// Atualiza o estoque de um produto
+// Pr?-condi??es: Nenhuma
+// P?s-condi??o: O estoque do produto ? atualizado
+void atualizarEstoque(ARQUIVOS files) {
+    printf("\n---> Operacao: Atualizar Estoque\n");
+    CABECALHO_INDICES * cab_indices = le_cabecalho_indices(files.file_indices);
+
+    if(cab_indices->pos_raiz == -1){
+        printf("---> A arvore esta vazia, nao e possivel realizar a operacao!\n");
+    } else {
+        int cod;
+        printf("---> Digite o codigo do produto de estoque a ser atualizado:\n");
+        scanf("%d", &cod);
+        atualizar_estoque_produto(files, cod);
+    }
+    free(cab_indices);
 }
 
-// Imprime a lista de nós livres do arquivo de índices
-// Pré-condições: Nenhuma
-// Pós-condição: A lista de nós livres do arquivo de índices é impressa no console
+// Imprime as informa??es de um produto espec?fico
+// Pr?-condi??es: Nenhuma
+// P?s-condi??o: As informa??es do produto s?o impressas no console
+void imprimirInformacoes(ARQUIVOS files) {
+    printf("\n---> Operacao: Imprimir informacoes de um produto\n");
+    CABECALHO_INDICES * cab_indices = le_cabecalho_indices(files.file_indices);
+
+    if(cab_indices->pos_raiz == -1){
+        printf("---> A arvore esta vazia, nao e possivel realizar a operacao!\n");
+    } else {
+        int codigo;
+        printf("---> Digite o codigo desejado para busca-lo: ");
+        scanf("%d", &codigo);
+        imprimir_informacoes_produto(files, codigo);
+    }
+    free(cab_indices);
+}
+
+// Imprime a lista de todos os produtos cadastrados
+// Pr?-condi??es: Nenhuma
+// P?s-condi??o: A lista de todos os produtos ? impressa no console
+void imprimirListaProdutos(ARQUIVOS files) {
+    printf("\n---> Operacao: Imprimir Lista de Produtos\n\n");
+    CABECALHO_INDICES * cab_indices = le_cabecalho_indices(files.file_indices);
+
+    if(cab_indices->pos_raiz == -1) {
+        printf("---> A arvore esta vazia, nao e possivel realizar a operacao!\n");
+    }else {
+        printf("+----------------------------------------------------------------------------------------------+\n");
+        printf("|                                         Lista de Produtos                                    |\n");
+        printf("+----------------------------------------------------------------------------------------------+\n");
+        printf("| CODIGO   | NOME                           | MARCA                     | ESTOQUE  | PRECO     |\n");
+        printf("+----------------------------------------------------------------------------------------------+\n");
+        imprimir_lista_produtos(files, cab_indices->pos_raiz);
+        printf("+----------------------------------------------------------------------------------------------+\n");
+    }
+    free(cab_indices);
+}
+
+// Imprime a ?rvore de produtos
+// Pr?-condi??es: Nenhuma
+// P?s-condi??o: A ?rvore de produtos ? impressa no console
+void imprimirArvore(ARQUIVOS files) {
+    // Implemente a l?gica para imprimir a ?rvore de produtos
+    printf("\n---> Operacao: Imprimir Arvore\n");
+    CABECALHO_INDICES * cab_indices = le_cabecalho_indices(files.file_indices);
+
+    if(cab_indices->pos_raiz == -1){
+        printf("---> A arvore esta vazia, nao e possivel realizar a operacao!\n");
+    } else {
+        imprimir_arvore(files);
+    }
+
+    free(cab_indices);
+}
+
+// Imprime a lista de n?s livres do arquivo de ?ndices
+// Pr?-condi??es: Nenhuma
+// P?s-condi??o: A lista de n?s livres do arquivo de ?ndices ? impressa no console
 void imprimirListaLivresIndices() {
-    printf("Operacao: Imprimir Lista de Livres do Arquivo de Indices\n");
-    // Implemente a lógica para imprimir a lista de livres do arquivo de índices
+    printf("\n---> Operacao: Imprimir Lista de Livres do Arquivo de Indices\n");
+    // Implemente a l?gica para imprimir a lista de livres do arquivo de ?ndices
 }
 
 // Imprime a lista de registros livres do arquivo de dados
-// Pré-condições: Nenhuma
-// Pós-condição: A lista de registros livres do arquivo de dados é impressa no console
+// Pr?-condi??es: Nenhuma
+// P?s-condi??o: A lista de registros livres do arquivo de dados ? impressa no console
 void imprimirListaLivresDados() {
-    printf("Operacao: Imprimir Lista de Livres do Arquivo de Dados\n");
-    // Implemente a lógica para imprimir a lista de livres do arquivo de dados
+    printf("\n---> Operacao: Imprimir Lista de Livres do Arquivo de Dados\n");
+    // Implemente a l?gica para imprimir a lista de livres do arquivo de dados
 }
 
-// Realiza operações em lote a partir de um arquivo de texto
-// Pré-condições: O arquivo de texto contendo as operações em lote deve existir e estar formatado corretamente
-// Pós-condição: As operações em lote são executadas
+// Realiza opera??es em lote a partir de um arquivo de texto
+// Pr?-condi??es: O arquivo de texto contendo as opera??es em lote deve existir e estar formatado corretamente
+// P?s-condi??o: As opera??es em lote s?o executadas
 void realizarOperacoesEmLote() {
-    printf("Operacao: Realizar Operacoes em Lote\n");
-    // Implemente a lógica para realizar operações em lote
+    printf("\n---> Operacao: Realizar Operacoes em Lote\n");
+    // Implemente a l?gica para realizar opera??es em lote
 }
 
 void imprimir_info_nos(ARQUIVOS files){
-    printf("\n----> Impressao de infos no no: \n");
+    printf("\n----> Operacao: Impressao de infos no no: \n");
     CABECALHO_INDICES * cab_indices = le_cabecalho_indices(files.file_indices);
-    imprimir_info_nos_chaves(files, cab_indices->pos_raiz);
+
+    if(cab_indices->pos_raiz == -1){
+        printf("---> A arvore esta vazia, nao e possivel realizar a operacao!\n");
+    } else {
+        imprimir_info_nos_chaves(files, cab_indices->pos_raiz);
+    }
+
+    free(cab_indices);
 }
 
-// Exibe o menu principal e processa a escolha do usuário
-// Pré-condições: Nenhuma
-// Pós-condição: O usuário escolhe uma opção do menu e a operação correspondente é executada
+// Exibe o menu principal e processa a escolha do usu?rio
+// Pr?-condi??es: Nenhuma
+// P?s-condi??o: O usu?rio escolhe uma op??o do menu e a opera??o correspondente ? executada
 void menu_principal(ARQUIVOS files){
     int opcao;
 
     do {
         header_menu();
-        printf("Escolha uma opcao: ");
+        printf("\n--> Escolha uma opcao: ");
         scanf("%d", &opcao);
 
         switch (opcao) {
@@ -176,10 +224,10 @@ void menu_principal(ARQUIVOS files){
                 imprimir_info_nos(files);
                 break;
             case 0:
-                printf("Saindo do programa. Ate mais!\n");
+                printf("---> Saindo do programa. Ate mais!\n");
                 break;
             default:
-                printf("Opcao invalida! Escolha uma opcao valida.\n");
+                printf("---> Opcao invalida! Escolha uma opcao valida.\n");
         }
     } while (opcao != 0);
 
